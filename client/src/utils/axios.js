12f41040dae6d46220075077fe62f3ef5f3b7ade
Utsav Mehta <lastk123456789@gmail.com>
2025-08-21 17:26:06 +0530

    Update frontend to use Railway backend URL directly

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import { logoutOn401 } from "./authUtils";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://yamanote.proxy.rlwy.net",
 });
 
 // Attach JWT to every request
